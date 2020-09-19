8e06069c778cd56cbe34ef43de365bb4eb234e84
Siza <siza2459@gmail.com>
2020-09-19 10:58:19 +0545

    Events pages added

---
+++ js/description.js
@@ -0,0 +1,17 @@
+function Gold()
+{
+    var message= document.getElementById("describe");
+    window.alert("We Serve you with HBD Ballons, Candles and the theme according to you!");
+}
+
+function Platinum()
+{
+    var message= document.getElementById("describe1");
+    window.alert("We Serve you with HBD Ballons, Scented Candles and design the environment according to you!");
+}
+
+function Silver()
+{
+    var message= document.getElementById("describe2");
+    window.alert("We Serve you with HBD Ballons,Romantic Candles and design the environment according to you!");
+}
